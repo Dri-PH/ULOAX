6045ba2ce8a3cbb31170b525f73a5310e0a9caec
Adriel <adrielpailos@gmail.com>
2021-10-26 21:14:06 -0300

    add header props

---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import * as S from "./styles";
+
+export function Header({
+    btncasa,
+    btneecc,
+    btnreserva,
+    btntaxi,
+    createdAt,
+    divparagraph,
+    logo,
+    paragraph,
+    paragraphagendeagr,
+    taxipng1,
+    taxipng2,
+    taxipng3,
+}) {
+    return(
+        <div>
+            <figure>
+                <img src={logo} alt="ULOAX" />
+            </figure>
+            <ul>
+                
+            </ul>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,45 @@
 import React from "react";
 import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+export const query = graphl`
+query {
+  headers {
+    btncasa
+    btneecc
+    btnreserva
+    btntaxi
+    createdAt
+    divparagraph
+    logo {
+      id
+      url
+    }
+    paragraph
+    paragraphagendeagr
+    taxipng1 {
+      id
+      url
+    }
+    taxipng2 {
+      id
+      url
+    }
+    taxipng3 {
+      id
+      url
+    }
+  }
+}
+`
+export default function Index({ data }) {
 
-export default function Index() {
+  console.log("Data", data)
+
   return (
     <div>
+      <Header 
+        logo={}
+      />
       <Footer />
     </div>
   );
